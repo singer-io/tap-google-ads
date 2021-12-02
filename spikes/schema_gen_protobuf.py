17048b448856e4929be8428dc7e1882b939e01f2
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2021-12-02 15:47:47 +0000

    Clean up, added comments to links where to look up missing classes, refactored out giant elif blocks

--- spikes/schema_gen_protobuf.py
+++ spikes/schema_gen_protobuf.py
@@ -10,19 +10,32 @@ from google.protobuf.pyext._message import RepeatedScalarContainer, RepeatedComp
 #>>> type(campaign.Campaign()._pb.target_spend.__class__)
 #<class 'google.protobuf.pyext.cpp_message.GeneratedProtocolMessageType'>
 
+# Unknown types lookup to their actual class in the code. For some reason these differ.
+# Unknown classes should be found in this project, likely somehwere around here:
+# https://github.com/googleads/google-ads-python/tree/14.1.0/google/ads/googleads/v9/common/types
 type_lookup = {"google.ads.googleads.v9.common.FinalAppUrl": "google.ads.googleads.v9.common.types.final_app_url.FinalAppUrl",
                "google.ads.googleads.v9.common.AdVideoAsset": "google.ads.googleads.v9.common.types.ad_asset.AdVideoAsset",
                "google.ads.googleads.v9.common.AdTextAsset": "google.ads.googleads.v9.common.types.ad_asset.AdTextAsset",
                "google.ads.googleads.v9.common.AdMediaBundleAsset": "google.ads.googleads.v9.common.types.ad_asset.AdMediaBundleAsset",
                "google.ads.googleads.v9.common.AdImageAsset": "google.ads.googleads.v9.common.types.ad_asset.AdImageAsset",
+
                "google.ads.googleads.v9.common.PolicyTopicEntry": "google.ads.googleads.v9.common.types.policy.PolicyTopicEntry",
                "google.ads.googleads.v9.common.PolicyTopicConstraint": "google.ads.googleads.v9.common.types.policy.PolicyTopicConstraint",
                "google.ads.googleads.v9.common.PolicyTopicEvidence": "google.ads.googleads.v9.common.types.policy.PolicyTopicEvidence",
-               "google.ads.googleads.v9.common.PolicyTopicConstraint.CountryConstraint": "google.ads.googleads.v9.common.types.policy.PolicyTopicConstraint",
+               "google.ads.googleads.v9.common.PolicyTopicConstraint.CountryConstraint": "google.ads.googleads.v9.common.types.policy.PolicyTopicConstraint", # This one's weird, handling it manually in the generator
+
                "google.ads.googleads.v9.common.UrlCollection": "google.ads.googleads.v9.common.types.url_collection.UrlCollection",
+
                "google.ads.googleads.v9.common.CustomParameter": "google.ads.googleads.v9.common.types.custom_parameter.CustomParameter",
+
                "google.ads.googleads.v9.common.ProductImage": "google.ads.googleads.v9.common.types.ad_type_infos.ProductImage",
-               "google.ads.googleads.v9.common.ProductVideo": "google.ads.googleads.v9.common.types.ad_type_infos.ProductVideo"}
+               "google.ads.googleads.v9.common.ProductVideo": "google.ads.googleads.v9.common.types.ad_type_infos.ProductVideo",
+
+               "google.ads.googleads.v9.common.FrequencyCapEntry": "google.ads.googleads.v9.common.types.frequency_cap.FrequencyCapEntry",
+
+               "google.ads.googleads.v9.common.TargetRestriction": "google.ads.googleads.v9.common.types.targeting_setting.TargetRestriction",
+               "google.ads.googleads.v9.common.TargetRestrictionOperation": "google.ads.googleads.v9.common.types.targeting_setting.TargetRestrictionOperation",
+               }
 
 def import_class(base_module_name):
     dir_path = os.path.dirname(os.path.realpath(base_path))
@@ -69,6 +82,52 @@ def type_to_json_schema(typ):
     else:
         raise Exception(f"Unknown scalar type {typ}")
 
+def handle_scalar_container(acc, prop_val, prop_camel):
+    try:
+        prop_val.append(1)
+        prop_val.append(True)
+        prop_val.append(0.0)
+    except TypeError as e:
+        re_result = re.search(r"expected (.+) got ", str(e))
+        if re_result:
+            actual_type = re_result.groups()[0]
+            mod = importlib.import_module('.'.join(actual_type.split('.')[:-1]))
+            obj = getattr(mod, actual_type.split('.')[-1])()
+            acc[prop_camel] = {"type": ["null", "array"],
+                               "items": get_schema({},obj._pb)}
+        else:
+            actual_types = re.search(r"but expected one of: (.+)$", str(e)).groups()[0].split(',')
+            actual_types = [t.strip() for t in actual_types]
+            acc[prop_camel] = {"type": ["null", "array"],
+                               "items": {"anyOf": [type_to_json_schema(t) for t in actual_types]}}
+
+
+def handle_composite_container(acc, prop_val, prop_camel):
+    try:
+        prop_val.append(1)
+        prop_val.append(True)
+        prop_val.append(0.0)
+    except TypeError as e:
+        re_result = re.search(r"expected (.+) got ", str(e))
+        if not re_result:
+            import ipdb; ipdb.set_trace()
+            1+1
+            raise
+        shown_type = re_result.groups()[0]
+        actual_type = type_lookup.get(shown_type)
+        if not actual_type:
+            print(f"Unknown composite type: {shown_type}")
+        else:
+            # TODO: Should we just build the objects based on the type name and insert them as a definition and $ref to save space?
+            mod = importlib.import_module('.'.join(actual_type.split('.')[:-1]))
+            if shown_type == "google.ads.googleads.v9.common.PolicyTopicConstraint.CountryConstraint":
+                obj = getattr(mod, actual_type.split('.')[-1]).CountryConstraint()
+            else:
+                obj = getattr(mod, actual_type.split('.')[-1])()
+            acc[prop_camel] = {"type": ["null", "array"],
+                               "items": get_schema({},obj._pb)}
+
+
 def get_schema(acc, current):
     for prop in filter(lambda p: re.search(r"^[a-z]", p), dir(current)):
         try:
@@ -91,49 +150,11 @@ def get_schema(acc, current):
             elif isinstance(prop_val, bytes):
                 # TODO: Should this just be empty? Then put it elsewhere to mark as unsupported? With a message?
                 # - Or should we just make it string?
-                acc[f"UNSUPPORTED?_{prop_camel}"] = {"type": ["null", "string"]}
+                acc[prop_camel] = {"type": ["null", "UNSUPPORTED_string"]}
             elif isinstance(prop_val, RepeatedScalarContainer):
-                try:
-                    prop_val.append(1)
-                    prop_val.append(True)
-                    prop_val.append(0.0)
-                except TypeError as e:
-                    re_result = re.search(r"expected (.+) got ", str(e))
-                    if re_result:
-                        actual_type = re_result.groups()[0]
-                        mod = importlib.import_module('.'.join(actual_type.split('.')[:-1]))
-                        obj = getattr(mod, actual_type.split('.')[-1])()
-                        acc[prop_camel] = {"type": ["null", "array"],
-                                           "items": get_schema({},obj._pb)}
-                    else:
-                        actual_types = re.search(r"but expected one of: (.+)$", str(e)).groups()[0].split(',')
-                        actual_types = [t.strip() for t in actual_types]
-                        acc[prop_camel] = {"type": ["null", "array"],
-                                           "items": {"anyOf": [type_to_json_schema(t) for t in actual_types]}}
+                handle_scalar_container(acc, prop_val, prop_camel)
             elif isinstance(prop_val, RepeatedCompositeContainer):
-                try:
-                    prop_val.append(1)
-                    prop_val.append(True)
-                    prop_val.append(0.0)
-                except TypeError as e:
-                    re_result = re.search(r"expected (.+) got ", str(e))
-                    if not re_result:
-                        import ipdb; ipdb.set_trace()
-                        1+1
-                        raise
-                    shown_type = re_result.groups()[0]
-                    actual_type = type_lookup.get(shown_type)
-                    if not actual_type:
-                        print(f"Unknown composite type: {shown_type}")
-                    else:
-                        # TODO: Should we just build the objects based on the type name and insert them as a definition and $ref to save space?
-                        mod = importlib.import_module('.'.join(actual_type.split('.')[:-1]))
-                        if shown_type == "google.ads.googleads.v9.common.PolicyTopicConstraint.CountryConstraint":
-                            obj = getattr(mod, actual_type.split('.')[-1]).CountryConstraint()
-                        else:
-                            obj = getattr(mod, actual_type.split('.')[-1])()
-                        acc[prop_camel] = {"type": ["null", "array"],
-                                           "items": get_schema({},obj._pb)}
+                handle_composite_container(acc, prop_val, prop_camel)
             else:
                 import ipdb; ipdb.set_trace()
                 1+1
@@ -152,5 +173,4 @@ with open("auto_ad_group.json", "w") as f:
     json.dump(get_schema({}, ad_group.AdGroup()._pb), f)
 with open("auto_account.json", "w") as f:
     json.dump(get_schema({}, customer.Customer()._pb), f)
-import ipdb; ipdb.set_trace()
-1+1
+print("Wrote schemas to local directory under auto_*.json, please review and manually set datetime formats.")

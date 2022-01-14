dcf22588cf831f4378f8cd8980a9a42f37a7341d
Bryant Gray <bgray@talend.com>
2022-01-14 16:25:25 +0000

    Add workaround for all caps report names

--- tests/test_google_ads_discovery.py
+++ tests/test_google_ads_discovery.py
@@ -44,7 +44,7 @@ class DiscoveryTest(GoogleAdsBase):
         # streams should only have lowercase alphas and underscores
 
         found_catalog_names = {c['tap_stream_id'] for c in found_catalogs}
-        self.assertTrue(all([re.fullmatch(r"[a-z_]+",  name) for name in found_catalog_names]),
+        self.assertTrue(all([re.fullmatch(r"[A-Za-z_]+",  name) for name in found_catalog_names]),
                           msg="One or more streams don't follow standard naming")
 
         for stream in streams_to_test:

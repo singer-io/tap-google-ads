adb1550c7a511684bbd4ca888572151d4b8d7089
dsprayberry <28106103+dsprayberry@users.noreply.github.com>
2022-03-11 17:07:05 +0000

    Mock singer.utils.now to return a specific date

--- tests/unittests/test_sync.py
+++ tests/unittests/test_sync.py
@@ -6,7 +6,8 @@ from unittest.mock import Mock
 from unittest.mock import patch
 from tap_google_ads.streams import ReportStream
 from tap_google_ads.streams import make_request
-
+import singer
+import pytz
 
 resource_schema = {
     "accessible_bidding_strategy": {
@@ -53,10 +54,17 @@ class TestEndDate(unittest.TestCase):
             {}
         )
 
+    @patch('singer.utils.now')
     @patch('tap_google_ads.streams.make_request')
-    def test_no_end_date(self, fake_make_request):
+    def test_no_end_date(self, fake_make_request, fake_datetime_now):
         start_date = datetime(2022, 1, 1, 0, 0, 0)
-        end_date = datetime.now()
+        end_date = datetime(2022, 3, 1, 0, 0, 0)
+
+        # Adding tzinfo helped the mock to work and avoids a
+        # TypeError(can't subtract offset-naive and offset-aware
+        # datetimes) here in the test
+        fake_datetime_now.return_value = end_date.replace(tzinfo=pytz.UTC)
+
         # Don't pass in end_date to test the tap's fallback to today
         self.run_sync(start_date, None, fake_make_request)
         all_queries_requested = self.get_queries_from_sync(fake_make_request)
@@ -67,11 +75,23 @@ class TestEndDate(unittest.TestCase):
         days_between_start_and_end = date_delta.days + 1
 
         # Compute the range of expected days, because end_date will always shift
-        expected_days = []
-        for i in range(days_between_start_and_end):
-            expected_datetime = start_date + timedelta(days=i)
-            expected_day = str(expected_datetime)[:10]
-            expected_days.append(expected_day)
+        expected_days = [
+            '2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04',
+            '2022-01-05', '2022-01-06', '2022-01-07', '2022-01-08',
+            '2022-01-09', '2022-01-10', '2022-01-11', '2022-01-12',
+            '2022-01-13', '2022-01-14', '2022-01-15', '2022-01-16',
+            '2022-01-17', '2022-01-18', '2022-01-19', '2022-01-20',
+            '2022-01-21', '2022-01-22', '2022-01-23', '2022-01-24',
+            '2022-01-25', '2022-01-26', '2022-01-27', '2022-01-28',
+            '2022-01-29', '2022-01-30', '2022-01-31', '2022-02-01',
+            '2022-02-02', '2022-02-03', '2022-02-04', '2022-02-05',
+            '2022-02-06', '2022-02-07', '2022-02-08', '2022-02-09',
+            '2022-02-10', '2022-02-11', '2022-02-12', '2022-02-13',
+            '2022-02-14', '2022-02-15', '2022-02-16', '2022-02-17',
+            '2022-02-18', '2022-02-19', '2022-02-20', '2022-02-21',
+            '2022-02-22', '2022-02-23', '2022-02-24', '2022-02-25',
+            '2022-02-26', '2022-02-27', '2022-02-28', '2022-03-01',
+        ]
 
         for day in expected_days:
             self.assertTrue(

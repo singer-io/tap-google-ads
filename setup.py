fb3e610fe00afa6a7096f7b1b10f3cd939d21c6a
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-03-22 15:23:50 +0200

    [CPL-8976] Use v13 for test

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='tap-google-ads',
           'singer-python==5.12.2',
           'requests>=2.26.0',
           'backoff==1.8.0',
-          'google-ads==19.0.0',
+          'google-ads==20.0.0',
           'protobuf==4.21.7',
           # Necessary to handle gRPC exceptions properly, documented
           # in an issue here: https://github.com/googleapis/python-api-core/issues/301

b80a66bdfa5876e7dae594955021f389921c59fd
Bryant Gray <bgray@talend.com>
2023-03-23 13:59:53 +0000

    Add log number

--- tap_google_ads/sync.py
+++ tap_google_ads/sync.py
@@ -130,7 +130,7 @@ def do_sync(config, catalog, resource_schema, state):
 
     # state.pop("currently_syncing", None)
 
-    for _ in range(1500000):
-        LOGGER.info("log size test")
+    for i in range(1500000):
+        LOGGER.info(f"log size test ({i})")
 
     singer.write_state(state)

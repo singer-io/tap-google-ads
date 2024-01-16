4e0d6f9df21bb0898697e1d253f05abdef67c760
Leslie VanDeMark <lvandemark@talend.com>
2024-01-16 15:37:03 +0000

    test on python 3.11

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(name='tap-google-ads',
-      version='1.6.0',
+      version='1.6.1',
       description='Singer.io tap for extracting data from the Google Ads API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_google_ads'],
       install_requires=[
-          'singer-python==5.12.2',
+          'singer-python==6.0.0',
           'requests==2.26.0',
           'backoff==1.8.0',
           'google-ads==22.1.0',
